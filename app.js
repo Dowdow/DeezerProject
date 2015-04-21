1c4936854b7a0ddab8f9b882d0c38dc41724f968
Léo Riera <leo.riera1@gmail.com>
2015-04-21 16:57:37 +0200

    Ajout track couchDB

--- app.js
+++ app.js
@@ -5,27 +5,44 @@ var app = express();
 var http = new HttpClient();
 
 app.get('/', function (request, response) {
-    var tracks = [];
-    http.GET('http://localhost:5984/deezer', function(resp) {
-        tracks = resp.body;
+    response.json('OK');
+});
+
+app.get('/search/:q', function (request, response) {
+    http.GET('http://api.deezer.com/search?q=' + request.params.q, function(resp) {
+        response.json(JSON.parse(resp.body).data);
     });
-    response.json(tracks);
 });
 
 app.get('/tracks', function (request, response) {
-    response.json('OK');
+    http.GET('http://localhost:5984/tracks', function(resp) {
+        response.json(JSON.parse(resp.body));
+    });
 });
 
 app.get('/tracks/:id', function (request, response) {
-    response.json('OK');
+    http.GET('http://localhost:5984/tracks', function(resp) {
+        response.json(JSON.parse(resp.body));
+    });
 });
 
 app.put('/tracks/:id', function (request, response) {
-    response.json('OK');
+    http.GET('http://api.deezer.com/track/' + request.params.id, function(resp) {
+        var track = JSON.parse(resp.body);
+        if(track.error) {
+            response.json('Track ' + request.params.id + ' does not exist');
+            return;
+        }
+        http.PUT('http://localhost:5984/tracks/' + request.params.id, resp.body, function(resp) {
+            response.json('OK');
+        });
+    });
 });
 
 app.delete('/tracks/:id', function (request, response) {
-    response.json('OK');
+    http.GET('http://localhost:5984/tracks', function(resp) {
+        response.json(JSON.parse(resp.body));
+    });
 });
 
 var server = app.listen(3000, function () {
