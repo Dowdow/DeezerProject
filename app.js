d36ad6bcf863ba25bbb00c04388325de1ae95ceb
Léo Riera <leo.riera1@gmail.com>
2015-04-21 18:12:13 +0200

    Track delete, get by id

--- app.js
+++ app.js
@@ -15,33 +15,48 @@ app.get('/search/:q', function (request, response) {
 });
 
 app.get('/tracks', function (request, response) {
-    http.GET('http://localhost:5984/tracks', function(resp) {
-        response.json(JSON.parse(resp.body));
-    });
+    response.json('OK');
 });
 
 app.get('/tracks/:id', function (request, response) {
-    http.GET('http://localhost:5984/tracks', function(resp) {
-        response.json(JSON.parse(resp.body));
+    http.GET('http://localhost:5984/tracks/' + request.params.id, function(resp) {
+        if(JSON.parse(resp.body).id) {
+            response.json(JSON.parse(resp.body)).status(200).end();
+        } else {
+            response.json('Error when retrieving the track').status(500).end();
+        }
     });
 });
 
 app.put('/tracks/:id', function (request, response) {
     http.GET('http://api.deezer.com/track/' + request.params.id, function(resp) {
-        var track = JSON.parse(resp.body);
-        if(track.error) {
-            response.json('Track ' + request.params.id + ' does not exist');
+        if(JSON.parse(resp.body).error) {
+            response.json('Track ' + request.params.id + ' does not exist').status(404).end();
             return;
         }
         http.PUT('http://localhost:5984/tracks/' + request.params.id, resp.body, function(resp) {
-            response.json('OK');
+            if(JSON.parse(resp.body).ok) {
+                response.json('OK').status(201).end();
+            } else {
+                response.json('Error when adding / updating the track').status(500).end();
+            }
         });
     });
 });
 
 app.delete('/tracks/:id', function (request, response) {
-    http.GET('http://localhost:5984/tracks', function(resp) {
-        response.json(JSON.parse(resp.body));
+    http.GET('http://localhost:5984/tracks/3135556', function(resp) {
+        if(!JSON.parse(resp.body).id) {
+            response.json('Error when retrieving tracks to delete').status(500).end();
+            return;
+        }
+        http.DELETE('http://localhost:5984/tracks/' + request.params.id + '?rev=' + JSON.parse(resp.body)._rev, function(resp) {
+            if(JSON.parse(resp.body).ok) {
+                response.json('OK').status(200).end();
+            } else {
+                response.json('Error when deleting the track').status(500).end();
+            }
+        });
     });
 });
 
